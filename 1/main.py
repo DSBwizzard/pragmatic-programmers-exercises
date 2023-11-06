d2befef934445d999ed81c1a962cf35e81d362a8
DSBwizzard <110507565+DSBwizzard@users.noreply.github.com>
2023-11-06 07:51:21 -0800

    Submit completed exercise 1

---
+++ 1/main.py
@@ -0,0 +1,12 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def say_hello():
+    print("Hello. What is your name?")
+    name = input()
+    print("It's nice to meet you, " + name + "!")
+
+say_hello()
